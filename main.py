fc9200073c8c4bdf53c1b56a6eedddee7a0b0e78
deadwoodfenceco-source <deadwoodfenceco@gmail.com>
2026-09-17 15:48:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("AlphaHawk Online")
+import os
+
+print("🦅 AlphaHawk Online")
+
+token = os.getenv("TELEGRAM_TOKEN")
+
+if token:
+    print("Token Loaded")
+else:
+    print("Token Missing")
+
